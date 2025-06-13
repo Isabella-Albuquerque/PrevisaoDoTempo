95a512c872e416d4c3e33e5b00d2108d9231c377
CavalcanteDebora <dcs.deborasantos@outlook.com>
2025-06-12 21:11:45 -0300

    feat(p2): cria componente de busca

---
+++ previsao-tempo/backend/index.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const PORTA = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/api/previsao', async (req, res) => {
+  const { cidade } = req.query;
+
+  if (!cidade) {
+    return res.status(400).json({ erro: 'É necessário digitar o nome da cidade no campo indicado' });
+  }
+
+  try {
+      const { data } = await axios.get('https://api.openweathermap.org/data/2.5/forecast', {
+      params: {
+        q: cidade,
+        appid: process.env.OPENWEATHER_API_KEY,
+        units: 'metric',
+        lang: 'pt_br',
+      },
+    });
+
+      const previsoes = data.list.map(previsao => ({
+      temp_min: previsao.main.temp_min,
+      temp_max: previsao.main.temp_max,
+      umidade: previsao.main.humidity,
+      icone: previsao.weather[0].icon,
+      descricao: previsao.weather[0].description,
+    }));
+
+
+    res.json(previsoes);
+  } catch (erro) {
+    console.error('Erro no backend:', erro.response?.data || erro.message || erro);
+    res.status(500).json({ erro: 'Erro ao buscar dados da previsão do tempo.' });
+  }
+});
+
+app.listen(PORTA, () => {
+  console.log(`inde.js rodando na porta: ${PORTA}`);
+});
--- previsao-tempo/src/frontend/components/Busca.jsx
+++ previsao-tempo/src/frontend/components/Busca.jsx
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { IconField } from 'primereact/iconfield';
-import { InputIcon } from 'primereact/inputicon';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-//componente busca, aqui deve ficar a lógica do input de dados. Depois precisa estilizar com ua classe .css
-
-// define um estado chamado 'cidade' e sua função de atualização, iniciando como uma string vazia
 const Busca = () => {
-  const [cidade, setCidade] = useState(''); 
-  
-  // função que realiza a busca da previsão do tempo na API quando chamada
-  const buscar = async () => {
-    try {
-  // faz uma requisição para a rota '/api/previsao', passando o nome da cidade como parâmetro
-      const { data } = await axios.get('/api/previsao', {
-        params: { cidade },
-      });
+    const [cidade, setCidade] = useState('São Paulo')
 
-      // exibe no console um título para os resultados da previsão do tempo
-      console.log('Resultados da Previsão do Tempo:');
-      // exibe cada previsão de forma formatada no console
-      data.forEach((previsao, index) => { //isso aqui fiz só pra exibir bonito no console, depois podemos apagar
-        console.log(`Previsão ${index + 1}:`); // soma 1 
-        console.log(`- Temperatura Mínima: ${previsao.temp_min} graus Celsius`);
-        console.log(`- Temperatura Máxima: ${previsao.temp_max} graus Celsius`);
-        console.log(`- Umidade: ${previsao.umidade}%`);
-        console.log(`- Descrição: ${previsao.descricao}`);
-        console.log('----------------------------------------');
-      });
-    } catch (error) {
-      console.error('Ocorreu um erro ao buscar a previsão:', error);
+    useEffect(() => {
+      const buscar = async () => {
+        try {
+          const { data } = await axios.get('/api/previsao', {
+            params: { cidade }
+        })
+
+          console.log('Resultados da Previsão do Tempo:')
+          
+          data.forEach((previsao, index) => { 
+            console.log(`Previsão ${index + 1}:`)  
+            console.log(`- Temperatura Mínima: ${previsao.temp_min} graus Celsius`)
+            console.log(`- Temperatura Máxima: ${previsao.temp_max} graus Celsius`)
+            console.log(`- Umidade: ${previsao.umidade}%`)
+            console.log(`- Descrição: ${previsao.descricao}`)
+            console.log('----------------------------------------')
+          })
+        } catch (error) {
+          console.error('Ocorreu um erro ao buscar a previsão:', error)
+        }
+  }
+  if(cidade.length < 3){
+      return 
     }
-  };
+    else{
+      const timeoutID = setTimeout(() => {
+        if(cidade)
+          buscar()
+      }, 2000)
+      return () => {
+        clearTimeout(timeoutID)
+      }
+    }
+}, [cidade]) 
 
-  return (
+ return (
     <div className="p-4">
       <h1 className="text-xl font-bold mb-4">Previsão do Tempo</h1>
-      <p className="mb-4">Digite o nome de uma cidade para obter as informações climáticas mais recentes:</p>
-      <IconField iconPosition="left">
-        <InputIcon className="pi pi-search" />
-        <InputText
-          placeholder="Nome da Cidade" //aqui precisaremos mudar pra deixar dafault São Paulo
-          value={cidade}
-          onChange={(e) => setCidade(e.target.value)}
-        />
-      </IconField>
-      <Button label="Buscar" className="ml-2" onClick={buscar} />
+      <p className="mb-4">Digite o nome de uma cidade para obter as informações climáticas dos próximos dias:</p>
+      <input 
+        type="text" 
+        id="CampoBusca" 
+        placeholder="Informe uma cidade" 
+        onChange={(e) => {setCidade(e.target.value)}}
+        value={cidade}
+        style={{paddingTop: 8, paddingBottom: 8, width: '60%', borderRadius: 6}}/>
     </div>
-  );
-};
+  )
+}
 
-export default Busca;
+export default Busca
